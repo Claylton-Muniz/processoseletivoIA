2e5879a1a269db6bd0f69d22442595f88f32f09e
Claylton-Muniz <claymuniz67@gmail.com>
2026-04-24 02:53:44 +0000

    Concluindo etapa 2, carregando o modelo pronto, convertendo e otimizando

--- optimize_model.py
+++ optimize_model.py
@@ -1,4 +1,16 @@
 import tensorflow as tf
 import os
 
-#insira seu código aqui
+# Carregando o modelo treinado
+model = tf.keras.models.load_model("model.h5")
+
+# Convertendo o modelo para o formato TensorFlow Lite
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+converter.optimizations = [tf.lite.Optimize.DEFAULT]  # Ativando otimizações
+tflite_model = converter.convert()
+
+# Salvando o modelo otimizado
+with open("model.tflite", "wb") as f:
+    f.write(tflite_model)
+
+print("Modelo otimizado salvo!!")
